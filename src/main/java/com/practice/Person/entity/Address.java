39d6c7b46f5ce8f3efc21d1d6886f6d3909333f1
Rafe Athar <rafeatharmateen@gmail.com>
2023-08-28 11:39:55 +0530

    added Address.java and changed Person.java

---
+++ src/main/java/com/practice/Person/entity/Address.java
@@ -0,0 +1,26 @@
+package com.practice.Person.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String county;
+    private String postalCode;
+}
--- src/main/java/com/practice/Person/entity/Person.java
+++ src/main/java/com/practice/Person/entity/Person.java
@@ -17,4 +17,7 @@ public class Person {
     private int id;
     private String firstName;
     private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
 }
